42bef542d41d2bb90bfc8a1f8ee494cf742ea818
Dax <daaawx@gmail.com>
2020-07-11 20:52:25 +0200

    Alter default output

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,9 @@
+const path = require('path');
+
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: 'app.js',
+    path: path.resolve(__dirname, 'build'),
   },
 };
